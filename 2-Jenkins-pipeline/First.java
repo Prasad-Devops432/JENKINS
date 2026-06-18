593e1528b7e09035a6579a84ba95aa7931f39945
Prasad <klprasad6@gmail.com>
2026-06-19 10:45:21 +1200

    Add First.java to print name and learning goals
    This program prints the user's name and learning goals.

---
+++ 2-Jenkins-pipeline/First.java
@@ -0,0 +1,8 @@
+// Question: Write a Java program to print your name and learning goal.
+
+public class First {
+    public static void main(String[] args) {
+        System.out.println("Hello, I am Lakshmi Prasad!");
+        System.out.println("Learning Cloud Computing, DevOps, and Cybersecurity.");
+    }
+}
